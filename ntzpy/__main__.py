0afcd7a51d3692f368bd6e9ed09e7f3257a0a89d
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-26 14:44:22 -0400

    app starts, can view version & 'help'

---
+++ ntzpy/__main__.py
@@ -0,0 +1,11 @@
+'''ntzpy entry point script'''
+
+from ntzpy import cli, __app_name__
+
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+
+if __name__=="__main__":
+    main()
---
+++ ntzpy/cli.py
@@ -0,0 +1,29 @@
+from typing import Optional
+import typer
+
+from ntzpy import __app_name__, __version__
+
+app = typer.Typer()
+
+
+def _version_callback(value):
+    if value:
+        typer.echo("-----------------------------ntzpy----------------------------")
+        typer.echo(f"{__app_name__} v{__version__}")
+        typer.echo("\nWhere notes are made and labs are completed")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
+
