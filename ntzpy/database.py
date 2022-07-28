c82dd92a0c30fb0fead9578e12788387058ecd73
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-28 13:49:52 -0400

    added to database reading & writing

--- ntzpy/database.py
+++ ntzpy/database.py
@@ -2,9 +2,44 @@
 """Provides database functionality for NTZ"""
 
 import configparser
+import json
 from pathlib import Path
+from typing import Any, Dict, List, NamedTuple
+from ntzpy import DB_READ_ERROR, DB_WRITE_ERROR, JSON_ERROR, SUCCESS
 
-from ntzpy import DB_WRITE_ERROR, SUCCESS
+
+# notes will be a list of dictionaries, each representing a to-do
+class DBResponse(NamedTuple):
+    todo_list: List[Dict[str, Any]]
+    error: int
+
+
+class DatabaseHandler:
+    # DatabaseHandler will do the reading & writing, when it's initialized it just needs to konw where this is all happening.
+    def __init__(self, db_path: Path) -> None:
+        self.db_path = db_path
+    # below to read & write to our JSON db.
+    # read_todos opens the db and deserializes it (takes from JSON format
+    # and turns it in to something we can natively use in Python).
+    def read_todos(self) -> DBResponse:
+        try:
+            with self.db_path.open("r") as db:
+                try:
+                    return DBResponse(json.load(db), SUCCESS)
+                except json.JSONDecodeError: # to catch wrong JSON format
+                    return DBResponse([], JSON_ERROR)
+        except OSError: # catch IO issues
+            return DBResponse( [], DB_READ_ERROR)
+
+    def write_todos(self, todo_list: List[Dict[str, Any]]) -> DBResponse:
+        """Takes a dictionary input to add to the database of to-dos"""
+        try:
+            with self.db_path.open("w") as db:
+                json.dump(todo_list, db, indent=4)
+            return DBResponse(todo_list, SUCCESS)
+        except OSError: #catch IO issues
+            return DBResponse(todo_list, DB_WRITE_ERROR)
+
 
 
 # if the user does not specify a save location for the database, this will generate a default.
--- ntzpy/ntz.py
+++ ntzpy/ntz.py
@@ -1,5 +1,11 @@
 # add your code in this file
 import os
+from typing import Any, Dict, NamedTuple
+
+
+class ToDo(NamedTuple):
+  todo: Dict[str, Any]
+  error: int
 
 # main function
 def cli():
